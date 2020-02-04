bcce95ea2e8dee26106f3152db253abc14152b38
Isma Baena <ismael.baena@cbre.com>
2020-02-04 13:56:32 +0100

    Llamada axios a API de valores dolar

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,7 +12,7 @@
           </v-date-picker>
         </v-card>
         <v-card color="error" dark>
-          <v-card-text class="display-1 text-center">650 - {{ fecha }}</v-card-text>
+          <v-card-text class="display-1 text-center">{{ valor }} - {{ fecha }}</v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -20,14 +20,27 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Main',
   data() {
       return {
           fecha: '',
           minimo: '1984',
-          maximo: new Date().toISOString().substr(0, 10)
+          maximo: new Date().toISOString().substr(0, 10),
+          valor: null
       }
+  },
+  methods: {
+      async getDolar(dia) {
+          let datos = await axios.get(`https://mindicador.cl/api/dolar/${dia}`);
+          console.log(datos.data.serie[0].valor);
+          this.valor = datos.data.serie[0].valor;
+      }
+  },
+  created() {
+      this.getDolar('01-02-2019');
   }
 }
 </script>
