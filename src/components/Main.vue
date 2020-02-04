d27140d48aadeb02b9e64f0d1ac1cb34ac4131ef
Isma Baena <ismael.baena@cbre.com>
2020-02-04 13:26:41 +0100

    Creación inicial de calendario

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,6 +2,14 @@
   <v-container>
     <v-row>
       <v-col>
+        <v-card class="text-center">
+          <v-date-picker v-model="fecha" full-width>
+
+          </v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">650 - {{ fecha }}</v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -9,6 +17,11 @@
 
 <script>
 export default {
-  name: "Main"
-};
+  name: 'Main',
+  data() {
+      return {
+          fecha: ''
+      }
+  }
+}
 </script>
